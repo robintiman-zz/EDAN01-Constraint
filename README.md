9440f72158b1f0092a715a05b07839ef75f4be8f
Robin Timan <robintiman@gmail.com>
2016-11-07 19:38:24 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# EDAN01-Constraint
---
+++ src/lab1/Burger.java
@@ -0,0 +1,34 @@
+package lab1;
+
+import org.jacop.constraints.XeqY;
+import org.jacop.constraints.XltC;
+import org.jacop.core.IntVar;
+import org.jacop.core.Store;
+import org.jacop.search.DepthFirstSearch;
+
+public class Burger {
+	
+	public Burger() {
+		Store store = new Store();
+		IntVar sod = new IntVar(store, "sod", 0,3000);
+		IntVar fat = new IntVar(store, "fat", 0,150);
+		IntVar cal = new IntVar(store, "cal", 0,3000);
+		IntVar ket = new IntVar(store, "ket");
+		IntVar let = new IntVar(store, "let");
+		IntVar pic = new IntVar(store, "pic");
+		IntVar tom = new IntVar(store, "tom");
+		IntVar cost;
+		
+		
+		store.impose(new XeqY(ket, let));
+		store.impose(new XeqY(pic, tom));
+		store.impose(new XltC(sod, 3000));
+		store.impose(new XltC(fat, 150));
+		store.impose(new XltC(cal, 3000));
+		
+		DepthFirstSearch<IntVar> dfs = new DepthFirstSearch<IntVar>();
+		
+		
+	}
+	
+}
