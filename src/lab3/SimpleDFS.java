7e88ab7a53b9107aa0422cf32218ec5c8086e846
David Norrestam <davidnorrestam@gmail.com>
2016-11-30 12:25:07 +0100

    Added Golomb

--- src/lab3/SimpleDFS.java
+++ src/lab3/SimpleDFS.java
@@ -46,50 +46,49 @@ import org.jacop.core.Store;
  * @version 4.1
  */
 
-public class SimpleDFS  {
+public class SimpleDFS {
 
-    boolean trace = true;
+	boolean trace = true;
 
-    /**
-     * Store used in search
-     */
-    Store store;
+	/**
+	 * Store used in search
+	 */
+	Store store;
 
-    /**
-     * Defines varibales to be printed when solution is found
-     */
-    IntVar[] variablesToReport;
+	/**
+	 * Defines varibales to be printed when solution is found
+	 */
+	IntVar[] variablesToReport;
 
-    /**
-     * It represents current depth of store used in search.
-     */
-    int depth = 0;
+	/**
+	 * It represents current depth of store used in search.
+	 */
+	int depth = 0;
 
-    /**
-     * It represents the cost value of currently best solution for FloatVar cost.
-     */
-    public int costValue = IntDomain.MaxInt;
+	/**
+	 * It represents the cost value of currently best solution for FloatVar cost.
+	 */
+	public int costValue = IntDomain.MaxInt;
 
-    /**
-     * It represents the cost variable.
-     */
-    public IntVar costVariable = null;
+	/**
+	 * It represents the cost variable.
+	 */
+	public IntVar costVariable = null;
 	int fails = 0, nbrNodes = 0;
 
-    public SimpleDFS(Store s) {
+	public SimpleDFS(Store s) {
 		store = s;
-    }
+	}
 
-
-    /**
-     * This function is called recursively to assign variables one by one.
-     */
-    public boolean label(IntVar[] vars) {
+	/**
+	 * This function is called recursively to assign variables one by one.
+	 */
+	public boolean label(IntVar[] vars) {
 		nbrNodes++;
 		if (trace) {
 			for (int i = 0; i < vars.length; i++)
-			System.out.print (vars[i] + " ");
-			System.out.println ();
+				System.out.print(vars[i] + " ");
+			System.out.println();
 		}
 
 		ChoicePoint choice = null;
@@ -99,12 +98,12 @@ public class SimpleDFS  {
 		// -1 since costValue is the cost of last solution
 		if (costVariable != null) {
 			try {
-			if (costVariable.min() <= costValue - 1)
-				costVariable.domain.in(store.level, costVariable, costVariable.min(), costValue - 1);
-			else
-				return false;
+				if (costVariable.min() <= costValue - 1)
+					costVariable.domain.in(store.level, costVariable, costVariable.min(), costValue - 1);
+				else
+					return false;
 			} catch (FailException f) {
-			return false;
+				return false;
 			}
 		}
 
@@ -159,89 +158,97 @@ public class SimpleDFS  {
 				}
 			}
 		}
-    }
-
-    void levelDown() {
-	store.removeLevel(depth);
-	store.setLevel(--depth);
-    }
-
-    void levelUp() {
-	store.setLevel(++depth);
-    }
-
-    void restoreLevel() {
-	store.removeLevel(depth);
-	store.setLevel(store.level);
-    }
-
-    public void reportSolution() {
-	if (costVariable != null)
-	    System.out.println ("Cost is " + costVariable);
-
-	for (int i = 0; i < variablesToReport.length; i++)
-	    System.out.print (variablesToReport[i] + " ");
-	System.out.println ("\n---------------");
-
-	System.out.println("Number of nodes: " + nbrNodes);
-	System.out.println("Number of failed: " + fails);
-    }
-
-    public void setVariablesToReport(IntVar[] v) {
-	variablesToReport = v;
-    }
-
-    public void setCostVariable(IntVar v) {
-	costVariable = v;
-    }
-
-    public class ChoicePoint {
-
-	IntVar var;
-	IntVar[] searchVariables;
-	int value;
-
-	public ChoicePoint (IntVar[] v) {
-	    var = selectVariable(v);
-	    value = selectValue(var);
 	}
 
-	public IntVar[] getSearchVariables() {
-	    return searchVariables;
+	void levelDown() {
+		store.removeLevel(depth);
+		store.setLevel(--depth);
 	}
 
-	/**
-	 * example variable selection; input order
-	 */
-	IntVar selectVariable(IntVar[] v) {
-	    if (v.length != 0) {
+	void levelUp() {
+		store.setLevel(++depth);
+	}
 
-		searchVariables = new IntVar[v.length-1];
-		for (int i = 0; i < v.length-1; i++) {
-		    searchVariables[i] = v[i+1];
+	void restoreLevel() {
+		store.removeLevel(depth);
+		store.setLevel(store.level);
+	}
+
+	public void reportSolution() {
+		if (costVariable != null)
+			System.out.println("Cost is " + costVariable);
+
+		for (int i = 0; i < variablesToReport.length; i++)
+			System.out.print(variablesToReport[i] + " ");
+		System.out.println("\n---------------");
+
+		System.out.println("Number of nodes: " + nbrNodes);
+		System.out.println("Number of failed: " + fails);
+	}
+
+	public void setVariablesToReport(IntVar[] v) {
+		variablesToReport = v;
+	}
+
+	public void setCostVariable(IntVar v) {
+		costVariable = v;
+	}
+
+	public class ChoicePoint {
+
+		IntVar var;
+		IntVar[] searchVariables;
+		int value;
+
+		public ChoicePoint(IntVar[] v) {
+			var = selectVariable(v);
+			value = selectValue(var);
 		}
 
-		return v[0];
+		public IntVar[] getSearchVariables() {
+			return searchVariables;
+		}
 
-	    }
-	    else {
-		System.err.println("Zero length list of variables for labeling");
-		return new IntVar(store);
-	    }
-	}
+		/**
+		 * example variable selection; input order
+		 */
+		IntVar selectVariable(IntVar[] v) {
+			if (v.length != 0) {
 
-	/**
-	 * example value selection; indomain_min
-	 */
-	int selectValue(IntVar v) {
-		return (v.min() + v.max()) / 2;
-	}
+				searchVariables = new IntVar[v.length - 1];
+				for (int i = 0; i < v.length - 1; i++) {
+					searchVariables[i] = v[i + 1];
+				}
+
+				return v[0];
+
+			} else {
+				System.err.println("Zero length list of variables for labeling");
+				return new IntVar(store);
+			}
+		}
+
+		/**
+		 * example value selection; indomain_min
+		 */
+		int selectValue(IntVar v) {
+			return (v.min() + v.max()) / 2;
+		}
 
 	/**
 	 * example constraint assigning a selected value
 	 */
 	public PrimitiveConstraint getConstraint() {
 	    return new XlteqC(var, value);
+		// return new XgteqC(var, value);
 	}
-    }
 }
+}
+
+
+
+
+
+
+
+
